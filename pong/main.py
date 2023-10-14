ca4aeb7070a87e494c914d0f40c89d1a89824a2b
Bstar00 <Samueloriola212@gmail.com>
2023-10-14 16:19:26 +0100

    My first python game

--- pong/main.py
+++ pong/main.py
@@ -77,34 +77,6 @@ def paddle2_down():
         y -= paddle2.dy
     paddle2.sety(y)
 
-# Function to move paddle1 up
-def paddle1_up():
-    y = paddle1.ycor()
-    if y < 250:
-        y += paddle1.dy
-    paddle1.sety(y)
-
-# Function to move paddle1 down
-def paddle1_down():
-    y = paddle1.ycor()
-    if y > -240:
-        y -= paddle1.dy
-    paddle1.sety(y)
-
-# Function to move paddle2 up
-def paddle2_up():
-    y = paddle2.ycor()
-    if y < 250:
-        y += paddle2.dy
-    paddle2.sety(y)
-
-# Function to move paddle2 down
-def paddle2_down():
-    y = paddle2.ycor()
-    if y > -240:
-        y -= paddle2.dy
-    paddle2.sety(y)
-
 # Keyboard bindings
 wn.listen()
 wn.onkeypress(paddle1_up, "w")
@@ -112,11 +84,10 @@ wn.onkeypress(paddle1_down, "s")
 wn.onkeypress(paddle2_up, "Up")
 wn.onkeypress(paddle2_down, "Down")
 
-# Update the score display
+# Function to update the score display
 def update_score():
     score_display.clear()
     score_display.write("Player 1: {}  Player 2: {}".format(score1, score2), align="center", font=("Arial", 24, "normal"))
-
 
 # Function to check paddle and ball collisions
 def check_collisions():
@@ -145,6 +116,25 @@ def auto_move_paddles():
             paddle2_up()
         elif paddle2.ycor() > ball.ycor():
             paddle2_down()
+
+# Set the score limit for the game over
+score_limit = 5  # Change this to the desired score limit
+
+# Function to check for game over
+def check_game_over():
+    global score1, score2
+    if score1 >= score_limit or score2 >= score_limit:
+        ball.goto(0, 0)
+        ball.dx = 0
+        ball.dy = 0
+        if score1 >= score_limit:
+            message = "Player 1 wins!"
+        else:
+            message = "Player 2 wins!"
+        message += "\nPlay Again? (Y/N)"
+        score_display.clear()
+        score_display.goto(0, 0)
+        score_display.write(message, align="center", font=("Arial", 24, "normal"))
 
 # Main game loop
 while True:
@@ -177,15 +167,5 @@ while True:
         update_score()
         check_collisions()
 
-    # Paddle and ball collisions
-    if (ball.dx > 0) and (350 > ball.xcor() > 340) and (paddle2.ycor() + 50 > ball.ycor() > paddle2.ycor() - 50):
-        ball.color("blue")
-        ball.setx(340)
-        ball.dx *= -1
-
-    if (ball.dx < 0) and (-350 < ball.xcor() < -340) and (paddle1.ycor() + 50 > ball.ycor() > paddle1.ycor() - 50):
-        ball.color("red")
-        ball.setx(-340)
-        ball.dx *= -1
- # Call the auto_move_paddles function
+    # Call the auto_move_paddles function
     auto_move_paddles()
